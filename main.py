76031b654e60b198af92f54429a5258ea654a107
mulbinde <mulbinde612@gmail.com>
2024-06-13 17:18:11 +0200

    count chars

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ def main():
     bpath = "books/frankenstein.txt"
     text = gettext(bpath)
     numw = cwords(text)
+    charc=cchar(text)
+    abc="abcdefghijklmnopqrstuvwxyz"
     print(f"{numw} words found in the document")
+    for i in abc:
+        print(f"{charc[i]} times letter {i} in the document")
 
 
 def cwords(text):
@@ -14,5 +18,15 @@ def gettext(path):
     with open(path) as f:
         return f.read()
 
+def lowerc(s):
+    return s.lower()
+
+def cchar(s):
+    dict={}
+    sl=lowerc(s)
+    for i in sl:
+        if not i in dict: dict[i]=1
+        else: dict[i]+=1
+    return dict
 
 main()
